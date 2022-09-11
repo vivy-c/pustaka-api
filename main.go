3e809b2b1596fb83652bb639e8a6b9859d29c1aa
Vivy Cahyani <vivy.cahyani@selarastech.com>
2022-09-11 22:49:53 +0700

    2. Handler

--- main.go
+++ main.go
@@ -11,20 +11,24 @@ func main() {
 	router := gin.Default()
 
 	//membuat request
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"name": "Vivy Cahyani",
-			"bio":  "Pelajar",
-		})
-	})
+	router.GET("/", rootHandler)
 
-	router.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"title":    "Hello World",
-			"subtitle": "Beajar golang nich",
-		})
-	})
+	router.GET("/hello", helloHandler)
 
 	//mengganti port route
 	router.Run(":8888")
+}
+
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Vivy Cahyani",
+		"bio":  "Pelajar",
+	})
+}
+
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"title":    "Hello World",
+		"subtitle": "Beajar golang nich",
+	})
 }
