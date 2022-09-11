59e8416825b32eb435f33b4f77bf58986263db04
Vivy Cahyani <vivy.cahyani@selarastech.com>
2022-09-11 23:12:22 +0700

    3. ath Variable dan Query String

--- main.go
+++ main.go
@@ -10,10 +10,11 @@ func main() {
 	//untuk iport pkg
 	router := gin.Default()
 
-	//membuat request
+	//membuat route
 	router.GET("/", rootHandler)
-
 	router.GET("/hello", helloHandler)
+	router.GET("/books/:id/:title", booksHandler)
+	router.GET("/query", queryHandler)
 
 	//mengganti port route
 	router.Run(":8888")
@@ -31,4 +32,19 @@ func helloHandler(c *gin.Context) {
 		"title":    "Hello World",
 		"subtitle": "Beajar golang nich",
 	})
+}
+
+func booksHandler(c *gin.Context) {
+	id := c.Param("id")
+	title := c.Param("title")
+
+	c.JSON(http.StatusOK, gin.H{"id": id, "title": title})
+}
+
+func queryHandler(c *gin.Context) {
+	title := c.Query("title")
+	//query string
+	price := c.Query("price")
+
+	c.JSON(http.StatusOK, gin.H{"title": title, "price": price})
 }
